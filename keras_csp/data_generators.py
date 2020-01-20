5605a056feff32788da49d59d5b675b567fa0736
Antonin Vobecky <a.vobecky@gmail.com>
2020-01-20 15:29:33 +0100

    Fix for loading blurred images.

--- keras_csp/data_generators.py
+++ keras_csp/data_generators.py
@@ -155,7 +155,6 @@ def get_data(ped_data, C, batchsize=8, exp_name=''):
                 img_data['filepath'] = img_data['filepath'].replace('images/', images_dir_name)
                 if 'blurred' in images_dir_name:
                     img_data['filepath'] = img_data['filepath'].replace('.png', '_blurred.jpg')
-                print(img_data['filepath'])
                 img_data, x_img = data_augment.augment(img_data, C)
                 if C.offset:
                     y_seman, y_height, y_offset = calc_gt_center(C, img_data, down=C.down, scale=C.scale, offset=True)
