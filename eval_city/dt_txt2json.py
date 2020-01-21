a85f9cb2dc3f24e84c4781df0e5237ed7a893395
vobecant <a.vobecky@gmail.com>
2020-01-21 11:26:46 +0100

    Added python version of conversion of results from txt files to json files.

--- eval_city/dt_txt2json.py
+++ eval_city/dt_txt2json.py
@@ -3,6 +3,18 @@ import os, sys
 import json
 import time
 
+"""
+Author: Antonin Vobecky
+Mail: a.vobecky@gmail.com, vobecant@fel.cvut.cz
+Github: https://github.com/vobecant
+LinkedIn: https://www.linkedin.com/in/antoninvobecky/
+
+Usage:
+    Python reimplementation of MATLAB code in dt_txt2json.m.
+    The script takes as the first argument a path to the folder with directories that contain detection results (corresponds to main_path).
+    If the path is not given, use default '../output/valresults/city/h/off'.
+    The script processes each detections file and saves its JSON version to the same folder.
+"""
 
 def txt2jsonFile(res):
     out_arr = []
@@ -43,7 +55,7 @@ if __name__ == '__main__':
             continue
         with open(dt_path, 'r') as f:
             res = f.readlines()
-        out_path = os.path.join(d, 'val_dt_py.json')
+        out_path = os.path.join(d, 'val_dt.json')
         if os.path.exists(out_path):
             print('File was already processed. Skipping...')
             continue
