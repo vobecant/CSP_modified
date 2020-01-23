48664481fbcce6cc2fb47e76b4dbb4b9035f7c31
Antonin Vobecky <a.vobecky@gmail.com>
2020-01-23 22:22:14 +0100

    Added testing of models finetuned on PP.

---
+++ test_finetune_pp.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+EXP_NAMES=( baseline, 1P, halfP, blurred)
+
+
+for EXP_NAME in "${EXP_NAMES[@]}"
+do
+    NET_NAMES=( exp2-${EXP_ORDINAL}_b1.0_equalize_gan exp2-${EXP_ORDINAL}_b1.0_equalize_pix2pixhd exp2-${EXP_ORDINAL}_b1.0_equalize_cpl)
+
+    EXPNAME="test_finetune_pp_${EXP_NAME}"
+    echo "Test experiment ${EXP_NAME}"
+
+
+    job_file="${EXPNAME}.job"
+    echo "run job_file ${job_file}"
+
+    echo "#!/bin/bash
+#SBATCH --job-name=$EXPNAME
+#SBATCH --output=${EXPNAME}.out
+#SBATCH --time=1-00:00:00
+#SBATCH --mem=30GB
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=16
+#SBATCH --partition=gpu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=a.vobecky@gmail.com
+
+python -u test_precarious_finetuned.py "${EXP_NAME}"
+
+cd /home/vobecant/PhD/CSP/eval_precarious
+python dt_txt2json.py /home/vobecant/PhD/CSP/output/valresults/precarious/h/off_${EXP_NAME}_finetuned
+
+cd eval_script
+python eval_precarious.py ${EXP_NAME}" > ${job_file}
+    sbatch ${job_file}
+
+	echo ""
+
+done
