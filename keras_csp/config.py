75330cd724fcad642c25aafad5b3080ff353ab2b
Antonin Vobecky <a.vobecky@gmail.com>
2020-01-31 16:09:18 +0100

    Changes in computation of validation MR-2.

--- keras_csp/config.py
+++ keras_csp/config.py
@@ -41,7 +41,7 @@ class TestConfig(object):
 		self.point = 'center' # or 'top', 'bottom
 		self.scale = 'h' # or 'w', 'hw'
 		self.num_scale = 1 # 1 for height (or width) prediction, 2 for height+width prediction
-		self.offset = False # append offset prediction or not
+		self.offset = True # append offset prediction or not
 		self.down = 4 # downsampling rate of the feature map for detection
 		self.radius = 2 # surrounding areas of positives for the scale map
 
