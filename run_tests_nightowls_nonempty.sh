0a89ba22cfef493ce0309b85681fd7f0847e5095
Antonin Vobecky <a.vobecky@gmail.com>
2020-05-30 10:13:21 +0200

    try deadline partition

--- run_tests_nightowls_nonempty.sh
+++ run_tests_nightowls_nonempty.sh
@@ -13,7 +13,7 @@ echo "#!/bin/bash -l
 #SBATCH --cpus-per-task=10
 #SBATCH --gres=gpu:1
 #SBATCH --partition gpu
-#SBATCH --exclude=node-10,node-12,node-01
+#SBATCH --exclude=node-10,node-12,node-01, node-16
 #SBATCH --mem=20GB
 #SBATCH --time=2-00:00:00
 
