e9e5cdbbc5d0b9b14af8edce645533122292ff7d
Antonin Vobecky <a.vobecky@gmail.com>
2020-05-31 16:08:54 +0200

    try deadline partition

--- run_train_city_orig.sh
+++ run_train_city_orig.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=csp_hard
-#SBATCH --output=csp_hard.err
+#SBATCH --job-name=csp_2P_hard
+#SBATCH --output=csp_hard_2P.err
 #SBATCH --time=3-00:00:00
 #SBATCH --mem=40GB
 #SBATCH --gres=gpu:Volta100:8
@@ -13,6 +13,6 @@
 #   CITYPERSON   #
 ##################
 # train the detector on Cityperson
-SPECIF=eccv_1P_hard_allTrain
-LR=0.0006
-python -u train_city_orig.py ${SPECIF} ${LR} >csp_hard.out
+SPECIF=eccv_2P_hard_allTrain
+LR=0.0008
+python -u train_city_orig.py ${SPECIF} ${LR} >csp_hard_2P.out
