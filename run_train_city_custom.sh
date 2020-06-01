739bf3e1137d227288e843a54a449fbf277c39f3
Antonin Vobecky <a.vobecky@gmail.com>
2020-06-02 00:02:46 +0200

    try deadline partition

---
+++ run_train_city_custom.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=csp_newInsertion
+#SBATCH --output=csp_newInsertion.err
+#SBATCH --time=3-00:00:00
+#SBATCH --mem=40GB
+#SBATCH --gres=gpu:Volta100:8
+#SBATCH --cpus-per-task=16
+#SBATCH --partition=gpu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=a.vobecky@gmail.com
+
+##################
+#   CITYPERSON   #
+##################
+# train the detector on Cityperson
+CACHEPATH="/home/vobecant/PhD/CSP/data/cache/cityperson_trainValTest/train_h50_eccv_1P_hard_resized"
+SPECIF=newInsertion
+LR=0.0008
+python -u train_city_orig.py ${CACHEPATH} ${SPECIF} ${LR} >csp_${SPECIF}.out
