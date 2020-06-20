548e97a44f517f5ea8850c85b48986b30a78cded
vobecant <a.vobecky@gmail.com>
2020-06-20 15:03:37 +0200

    Run training with occluded extended dataset.

--- run_train_city_custom.sh
+++ run_train_city_custom.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=csp_occluded
-#SBATCH --output=csp_1P_occluded.err
+#SBATCH --job-name=csp_occluded_fixedRatio
+#SBATCH --output=csp_1P_occluded_fixedRatio.err
 #SBATCH --time=3-00:00:00
 #SBATCH --mem=40GB
 #SBATCH --gres=gpu:Volta100:8
@@ -13,7 +13,7 @@
 #   CITYPERSON   #
 ##################
 # train the detector on Cityperson
-CACHEPATH="/home/vobecant/PhD/CSP/data/cache/cityperson_trainValTest/train_h50_occluded_1P_occluded_dgx"
-SPECIF=1P_occluded
+CACHEPATH="/home/vobecant/PhD/CSP/data/cache/cityperson_trainValTest/train_h50_1P_occluded_fixedRatio_dgx"
+SPECIF=1P_occluded_fixedRatio
 LR=0.0008
 python -u train_city_custom.py ${CACHEPATH} ${SPECIF} ${LR} >csp_${SPECIF}.out
