69205f250df1f5f0e5abf3c65cb468c8cafd30e5
vobecant <a.vobecky@gmail.com>
2020-06-22 16:44:28 +0200

    Train detector with reasonable annotations only.

--- run_train_city_custom.sh
+++ run_train_city_custom.sh
@@ -2,9 +2,9 @@
 #SBATCH --job-name=csp_baseline_reasonable
 #SBATCH --output=csp_baseline_reasonable.err
 #SBATCH --time=3-00:00:00
-#SBATCH --mem=120GB
+#SBATCH --mem=190GB
 #SBATCH --gres=gpu:Volta100:8
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=8
 #SBATCH --partition=gpu
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=a.vobecky@gmail.com
