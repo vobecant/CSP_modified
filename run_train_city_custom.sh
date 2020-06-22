ba05d7baec26d0619ca5f06f51a1a2c7215e04e2
vobecant <a.vobecky@gmail.com>
2020-06-22 15:33:32 +0200

    Generate training annotations for the reasonable subset only.

--- run_train_city_custom.sh
+++ run_train_city_custom.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=csp_occluded_fixedRatio
-#SBATCH --output=csp_1P_occluded_fixedRatio.err
+#SBATCH --job-name=csp_baseline_reasonable
+#SBATCH --output=csp_baseline_reasonable.err
 #SBATCH --time=3-00:00:00
 #SBATCH --mem=40GB
 #SBATCH --gres=gpu:Volta100:8
@@ -13,7 +13,7 @@
 #   CITYPERSON   #
 ##################
 # train the detector on Cityperson
-CACHEPATH="/home/vobecant/PhD/CSP/data/cache/cityperson_trainValTest/train_h50_1P_occluded_fixedRatio_dgx"
-SPECIF=1P_occluded_fixedRatio
+CACHEPATH="/home/vobecant/PhD/CSP/data/cache/cityperson/train_h50_reasonable"
+SPECIF=baseline_reasonable
 LR=0.0008
 python -u train_city_custom.py ${CACHEPATH} ${SPECIF} ${LR} >csp_${SPECIF}.out
