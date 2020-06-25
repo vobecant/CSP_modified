eaf58ab13702b54f3ef5526eb079e45b31909a8c
vobecant <a.vobecky@gmail.com>
2020-06-25 12:00:04 +0200

    Improvements in analysing the test set.

--- show_missed_detections.py
+++ show_missed_detections.py
@@ -178,7 +178,7 @@ fig, ax = plt.subplots(tight_layout=True)
 hist = ax.hist2d(height_reasonable, vis_reasonable,
                  bins=[np.arange(50, max(height_reasonable), 10), np.arange(0.65, 1.0, 0.05)])
 plt.title('Visibility and height of all reasonable.')
-fig.colorbar(hist)
+plt.colorbar(hist[3],ax=ax)
 plt.savefig(os.path.join(save_dir_plots, 'heightVis_hist_all_reasonable.jpg'))
 plt.close()
 
@@ -186,7 +186,7 @@ fig, ax = plt.subplots(tight_layout=True)
 hist = ax.hist2d(height_occluded, vis_occluded,
                  bins=[np.arange(50, max(height_occluded), 10), np.arange(0, 0.65, 0.05)])
 plt.title('Visibility and height of all occluded.')
-fig.colorbar(hist)
+plt.colorbar(hist[3],ax=ax)
 plt.savefig(os.path.join(save_dir_plots, 'heightVis_hist_all_occluded.jpg'))
 plt.close()
 
