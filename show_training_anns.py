2f2f1a41bb3d4d7b90d282e2adca4653d8d20f8a
vobecant <a.vobecky@gmail.com>
2020-06-25 12:59:56 +0200

    Improvements in analysing the test set.

--- show_training_anns.py
+++ show_training_anns.py
@@ -141,7 +141,7 @@ fig, axs = plt.subplots(1, 2, tight_layout=True)
 n_bins = 40
 axs[0].hist(heights, bins=n_bins, cumulative=True, density=True)
 axs[0].set_title('training heights')
-axs[1].hist(-visibilities, bins=n_bins, cumulative=True, density=True)
+axs[1].hist(-np.asarray(visibilities), bins=n_bins, cumulative=True, density=True)
 axs[1].set_title('training visibilities')
 plt.savefig(os.path.join(save_dir_plots, 'trn_hists_all_cumulative.jpg'))
 plt.close()
