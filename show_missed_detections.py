fe018827888fd08911097cea4ca6919f16a11135
vobecant <a.vobecky@gmail.com>
2020-06-26 10:07:55 +0200

    Improvements in analysing the test set.

--- show_missed_detections.py
+++ show_missed_detections.py
@@ -100,7 +100,7 @@ save_dir = sys.argv[3]
 save_dir_missed = os.path.join(save_dir, 'missed')
 save_dir_plots = os.path.join(save_dir, 'plots')
 dt_gt_file = sys.argv[4]  # /home/vobecant/PhD/CSP/data/cache/cityperson_trainValTest/train_h50_eccv_1P_hard_allTrain
-NO_PLOT = len(sys.argv)>5
+NO_PLOT = len(sys.argv) > 5
 
 if not os.path.exists(save_dir):
     os.makedirs(save_dir)
@@ -442,3 +442,16 @@ plt.title('Visibility and height of missed occluded.')
 plt.colorbar(hist[3], ax=ax)
 plt.savefig(os.path.join(save_dir_plots, 'test_heightVis_hist_missed_occluded.jpg'))
 plt.close()
+
+data = {
+    'missed_occluded_height': missed_occluded_height,
+    'missed_occluded_visibility': missed_occluded_visibility,
+    'missed_reasonable_height': missed_reasonable_height,
+    'missed_reasonable_visibility': missed_reasonable_visibility,
+    'height_occluded': height_occluded,
+    'vis_occluded': vis_occluded,
+    'height_reasonable': height_reasonable,
+    'vis_reasonable': vis_reasonable,
+}
+with open('./cityperson_analysis/test_statistics.pkl','wb') as f:
+    pickle.dump(data,f)
