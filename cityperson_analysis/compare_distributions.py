9b987bc4d11f1731e79426d6733266278f765388
vobecant <a.vobecky@gmail.com>
2020-06-26 12:01:11 +0200

    Improvements in analysing the test set.

--- cityperson_analysis/compare_distributions.py
+++ cityperson_analysis/compare_distributions.py
@@ -92,3 +92,56 @@ ax.legend()
 ax.set_title('All train vs occluded test')
 plt.savefig('./test_occ_vs_train.jpg')
 plt.close()
+
+# TODO: cropped versions with limited max height to 300
+MAX_HEIGHT=300
+keep_idx = xedges_trn<=MAX_HEIGHT
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+hist_trn, xedges_trn, yedges_trn = np.histogram2d(train_heights, train_visibilities, bins=[
+    np.arange(50, max(max(train_heights), max(test_heights_all)), 10), np.arange(0, 1.0, 0.05)], density=True)
+hist_tst, xedges_tst, yedges_tst = np.histogram2d(test_heights_all, test_visibilities_all, bins=[
+    np.arange(50, max(max(train_heights), max(test_heights_all)), 10), np.arange(0, 1.0, 0.05)], density=True)
+
+x_trn, y_trn = np.meshgrid(xedges_trn[keep_idx], yedges_trn[:-1], indexing="ij")
+z_trn = hist_trn[keep_idx]
+ax.plot_wireframe(x_trn, y_trn, z_trn, color='blue', label='all train')
+
+x_tst, y_tst = np.meshgrid(xedges_tst[keep_idx], yedges_tst[:-1], indexing="ij")
+z_tst = hist_tst[keep_idx]
+ax.plot_wireframe(x_tst, y_tst, z_tst, color='red', label='all test')
+ax.legend()
+
+ax.set_title('All train vs all test, cropped.')
+
+plt.savefig('./test_all_vs_train_crop.jpg')
+plt.close()
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+hist_tst_r, xedges_tst_r, yedges_tst_r = np.histogram2d(test_heights_reasonable, test_visibilities_reasonable, bins=[
+    np.arange(50, max(max(train_heights), max(test_heights_reasonable)), 10), np.arange(0, 1.0, 0.05)], density=True)
+ax.plot_wireframe(x_trn, y_trn, z_trn, color='blue', label='all train')
+
+x_tst, y_tst = np.meshgrid(xedges_tst_r[keep_idx], yedges_tst_r[:-1], indexing="ij")
+z_tst = hist_tst_r[keep_idx]
+ax.plot_wireframe(x_tst, y_tst, z_tst, color='red', label='reasonable test')
+ax.legend()
+ax.set_title('All train vs reasonable test, cropped.')
+plt.savefig('./test_reason_vs_train_crop.jpg')
+plt.close()
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+hist_tst_o, xedges_tst_o, yedges_tst_o = np.histogram2d(test_heights_occluded, test_visibilities_occluded, bins=[
+    np.arange(50, max(max(train_heights), max(test_heights_occluded)), 10), np.arange(0, 1.0, 0.05)], density=True)
+ax.plot_wireframe(x_trn, y_trn, z_trn, color='blue', label='all train')
+
+x_tst, y_tst = np.meshgrid(xedges_tst_o[keep_idx], yedges_tst_o[:-1], indexing="ij")
+z_tst = hist_tst_o[keep_idx]
+ax.plot_wireframe(x_tst, y_tst, z_tst, color='red', label='occluded test')
+ax.legend()
+ax.set_title('All train vs occluded test, cropped.')
+plt.savefig('./test_occ_vs_train_crop.jpg')
+plt.close()
