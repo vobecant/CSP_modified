39e7415bd479495e6a307e96a89704d807f1ccc9
vobecant <a.vobecky@gmail.com>
2020-06-26 16:43:05 +0200

    Show missed detections based on COCO api code.

--- eval_city/eval_script/eval_demo_allTrain.py
+++ eval_city/eval_script/eval_demo_allTrain.py
@@ -44,6 +44,57 @@ annFile = '/home/vobecant/PhD/CSP/eval_city/val_gt.json'
 img_base = '/home/vobecant/datasets/cityscapes/leftImg8bit/val'
 base_save_dir = './missed_detections'
 main_path = sys.argv[1]
+
+
+def xywh2xyxy(x):
+    # Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
+    y = np.zeros_like(x)
+    y[0] = x[0]
+    y[1] = x[1]
+    y[2] = x[0] + x[2]  # bottom right x
+    y[3] = x[1] + x[3]  # bottom right y
+    return y
+
+
+def save_crop(bb, image, save_file):
+    side = int(max(bb[-2:]) * 1.5)
+    x_c = bb[0] + bb[2] // 2
+    y_c = bb[1] + bb[3] // 2
+
+    left = max(0, x_c - side // 2)
+    top = max(0, y_c - side // 2)
+    right = min(2047, left + side)
+    bottom = min(1023, top + side)
+
+    crop = image[top:bottom, left:right]
+    cv2.imwrite(save_file, crop)
+
+
+def plot_bbs(image, image_name, bbs, vis, save_dir, color):
+    # TODO: plot whole image
+    for i, (bb, v) in enumerate(zip(bbs, vis)):
+        bb_xyxy = xywh2xyxy(bb)
+        plot_one_box(bb_xyxy, image, color, v)
+        # TODO: save crop of the missed sample
+        save_file_crop = os.path.join(save_dir, image_name + '_{}.png'.format(i))
+        save_crop(bb, image, save_file_crop)
+    save_file_scene = os.path.join(save_dir, image_name + '.jpg')
+    cv2.imwrite(save_file_scene, image)
+
+
+def plot_one_box(bb, img, color, label=None, line_thickness=None):
+    # Plots one bounding box on image img
+    tl = line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
+    c1, c2 = (int(bb[0]), int(bb[1])), (int(bb[2]), int(bb[3]))
+    cv2.rectangle(img, c1, c2, color, thickness=tl)
+    if label:
+        tf = max(tl - 1, 1)  # font thickness
+        t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
+        c1 = c2[0] - t_size[0], c2[1] + t_size[1] + 3
+        cv2.rectangle(img, c1, c2, color, -1)  # filled
+        cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+
+
 if os.path.isfile(main_path):
     print('Given file {} with detections'.format(main_path))
     res_file = None
@@ -51,25 +102,26 @@ if os.path.isfile(main_path):
     resFile = os.path.join(main_path.replace('.txt', '.json'))
     for id_setup in range(6):
         cocoGt = COCO(annFile)
-        setup_name = ''
         img_lut = {img['id']: img for img in cocoGt.imgs.values()}
         ann_lut = {ann['id']: ann for ann in cocoGt.anns.values()}
         cocoDt = cocoGt.loadRes(resFile)
         imgIds = sorted(cocoGt.getImgIds())
         cocoEval = COCOeval(cocoGt, cocoDt, annType)
+        setup_name = cocoEval.eval['params'].SetupLbl[id_setup]
         cocoEval.params.imgIds = imgIds
         cocoEval.evaluate(id_setup)
         misses = cocoEval.accumulate(plot=True, return_misses=True)
         mean_mr = cocoEval.summarize(id_setup, res_file)
         for img_id, ms in misses.items():
             if len(ms):
-                image_name = img_lut[img_id]
+                image_name = img_lut[img_id]['im_name']
                 city = image_name.split('_')[0]
                 image_path = os.path.join(img_base, city, image_name)
                 image = cv2.imread(image_path)
                 bbs = [ann_lut[m]['bbox'] for m in ms]
                 vis = [ann_lut[m]['vis_ratio'] for m in ms]
-                plot_bbs(image,bbs,vis,os.path.join(base_save_dir,setup_name))
+                plot_bbs(image, image_name.split('.'), bbs, vis, os.path.join(base_save_dir, setup_name),
+                         color=(255, 0, 0))
         if id_setup == 0:
             mr_reasonable = mean_mr
     print('')
