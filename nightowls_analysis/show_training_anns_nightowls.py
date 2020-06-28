0515fd66d00c0217e4369e3c60bb8bc257c9dff9
vobecant <a.vobecky@gmail.com>
2020-06-28 23:18:28 +0200

    Comparison of training and test distributions.

--- nightowls_analysis/show_training_anns_nightowls.py
+++ nightowls_analysis/show_training_anns_nightowls.py
@@ -10,10 +10,59 @@ import pickle
 
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-from ..eval_nightowls.eval_single import plot_bbs
-
 
 def xywh2xyxy(x):
+    # Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
+    y = np.zeros_like(x)
+    y[0] = x[0]
+    y[1] = x[1]
+    y[2] = x[0] + x[2]  # bottom right x
+    y[3] = x[1] + x[3]  # bottom right y
+    return y
+
+
+def save_crop(bb, image, save_file):
+    side = int(max(bb[-2:]) * 1.5)
+    x_c = bb[0] + bb[2] // 2
+    y_c = bb[1] + bb[3] // 2
+
+    left = max(0, x_c - side // 2)
+    top = max(0, y_c - side // 2)
+    right = min(2047, left + side)
+    bottom = min(1023, top + side)
+
+    crop = image[top:bottom, left:right]
+    cv2.imwrite(save_file, crop)
+
+
+def plot_bbs(image, image_name, bbs, vis, heights, save_dir, color):
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    # TODO: plot whole image
+    for i, (bb, v, h) in enumerate(zip(bbs, vis, heights)):
+        bb_xyxy = xywh2xyxy(bb)
+        plot_one_box(bb_xyxy, image, color, 'v{:.2f}|h{}'.format(v, h))
+        # TODO: save crop of the missed sample
+        save_file_crop = os.path.join(save_dir, image_name + '_{}.png'.format(i))
+        save_crop(bb, image, save_file_crop)
+    save_file_scene = os.path.join(save_dir, image_name + '.jpg')
+    cv2.imwrite(save_file_scene, image)
+
+
+def plot_one_box(bb, img, color, label=None, line_thickness=None):
+    # Plots one bounding box on image img
+    tl = line_thickness or 1  # line/font thickness
+    c1, c2 = (int(bb[0]), int(bb[1])), (int(bb[2]), int(bb[3]))
+    cv2.rectangle(img, c1, c2, color, thickness=tl)
+    if label:
+        tf = max(tl - 1, 1)  # font thickness
+        t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
+        c1 = c2[0] - t_size[0], c2[1] + t_size[1] + 3
+        cv2.rectangle(img, c1, c2, color, -1)  # filled
+        cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+
+
+def xywh2xyxy_multi(x):
     # Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
     y = np.zeros_like(x)
     y[:, 0] = x[:, 0] - x[:, 2] / 2  # top left x
