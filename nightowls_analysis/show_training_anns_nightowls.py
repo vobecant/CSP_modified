767ef241af88729643f9e475f7a376697a3d45d3
vobecant <a.vobecky@gmail.com>
2020-06-28 23:20:41 +0200

    Comparison of training and test distributions.

--- nightowls_analysis/show_training_anns_nightowls.py
+++ nightowls_analysis/show_training_anns_nightowls.py
@@ -170,7 +170,7 @@ for i, ann in enumerate(anns):
         print('Skip {}'.format(i))
         continue
     image = cv2.cvtColor(cv2.imread(image_name), cv2.COLOR_BGR2RGB)
-    plot_bbs(image, None, bbs_gt, vis, heights, save_dir, color_gt)
+    plot_bbs(image, image_name.split('.')[0], bbs_gt, vis, heights, save_dir, color_gt)
     # img_gts = plot_images(image, bbs_gt, None, image_name, label='GT', gt=True, color=color_gt, tlg=1)
     # pth = os.path.join(save_dir, 'im{}_dets.png'.format(i + 1))
     # print('Saved GTs to {}'.format(pth))
