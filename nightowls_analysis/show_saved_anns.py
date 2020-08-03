1daa922e28191a5ea9e4f1bcd93fa715be7c6c9e
vobecant <a.vobecky@gmail.com>
2020-08-03 20:58:11 +0200

    Change back number of iterations per epoch.

---
+++ nightowls_analysis/show_saved_anns.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import cv2
+from PIL import Image
+
+DIR = sys.argv[1]
+
+imgs = [im for im in os.listdir(DIR) if '_' not in im]
+
+for im in imgs:
+    full_fname = os.path.join(DIR, im)
+    crop_fname, ext = im.split('.')
+    crop_fname = crop_fname + '_0.' + ext
+    crop_fname = os.path.join(DIR, crop_fname)
+    full = Image.open(full_fname)
+    crop = Image.open(crop_fname)
+    full.show()
+    crop.show()
+
