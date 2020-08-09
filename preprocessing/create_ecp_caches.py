2066ccb819d883b4f6cf178786b8c988757963ca
vobecant <a.vobecky@gmail.com>
2020-08-09 17:24:11 +0200

    Initialize nightowls experiment from CityScapes experiment.

--- preprocessing/create_ecp_caches.py
+++ preprocessing/create_ecp_caches.py
@@ -6,16 +6,17 @@ from pathlib import Path
 import numpy as np
 
 DATASET_DIR = sys.argv[1]  # /home/vobecant/datasets/ecp/ECP
-SAVE_FILE = sys.argv[2]  # /home/vobecant/datasets/ecp/ECP
+SAVE_DIR = sys.argv[2]  # /home/vobecant/datasets/ecp/ECP
 TRN_ANN_DIR = os.path.join(DATASET_DIR, 'day', 'labels', 'train')
 VAL_ANN_DIR = os.path.join(DATASET_DIR, 'day', 'labels', 'val')
 
 trn_files = [os.path.join(TRN_ANN_DIR, f) for f in Path(TRN_ANN_DIR).rglob('*.json')]
 val_files = [os.path.join(VAL_ANN_DIR, f) for f in Path(VAL_ANN_DIR).rglob('*.json')]
 
-d = os.path.split(SAVE_FILE)[0]
-if not os.path.exists(d):
-    os.makedirs(d)
+if not os.path.exists(SAVE_DIR):
+    os.makedirs(SAVE_DIR)
+save_file_trn = os.path.join(SAVE_DIR, 'train_h50')
+save_file_val = os.path.join(SAVE_DIR, 'val_h50')
 
 '''
 Annotations for CSP training are in the form of list of dictionaries. Each such dict contains:
@@ -25,8 +26,39 @@ Annotations for CSP training are in the form of list of dictionaries. Each such 
 'filepath'
 '''
 
+# TODO: train files
 anns_list = []
 for trnf in trn_files:
+    with open(trnf, 'r') as f:
+        trn = json.load(f)
+
+    img_path = trnf.replace('.json', '.png').replace('/labels/', '/img/')
+    assert os.path.exists(img_path)
+
+    ann = {'ignoreareas': [],
+           'vis_bboxes': np.asarray([]),
+           'bboxes': [],
+           'filepath': img_path}
+
+    for ann in trn['children']:
+        bbox = [ann['x0'], ann['y0'], ann['x1'], ann['y1']]
+        height = bbox[-1] - bbox[1]
+        if ann['identity'] == 'pedestrian' and height >= 50:
+            ann['bboxes'].append(bbox)
+        else:
+            ann['ignoreareas'].append(bbox)
+
+    ann['bboxes'] = np.asarray(ann['bboxes'])
+    ann['ignoreareas'] = np.asarray(ann['ignoreareas'])
+
+    anns_list.append(ann)
+
+with open(save_file_trn, 'rb') as f:
+    pickle.dump(anns_list, f)
+
+# TODO: validation files
+anns_list = []
+for trnf in val_files:
     with open(trnf, 'r') as f:
         trn = json.load(f)
 
@@ -50,5 +82,5 @@ for trnf in trn_files:
 
     anns_list.append(ann)
 
-with open(SAVE_FILE, 'rb') as f:
+with open(save_file_val, 'rb') as f:
     pickle.dump(anns_list, f)
