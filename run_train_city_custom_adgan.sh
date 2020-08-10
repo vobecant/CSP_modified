e03cb06de79b9b0e9441b20246e38cbc4507d690
vobecant <a.vobecky@gmail.com>
2020-08-10 15:51:48 +0200

    Script for running detection on ECP dataset.

---
+++ run_train_city_custom_adgan.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=cp_adgan
+#SBATCH --output=cp_adgan.err
+#SBATCH --time=3-00:00:00
+#SBATCH --mem=190GB
+#SBATCH --gres=gpu:Volta100:8
+#SBATCH --cpus-per-task=16
+#SBATCH --partition=gpu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=a.vobecky@gmail.com
+
+##################
+#   CITYPERSON   #
+##################
+# train the detector on Cityperson
+CACHEPATH="/home/vobecant/PhD/CSP/data/cache/cityperson/train_h50_adgan"
+SPECIF=cp_adgan
+LR=0.0006
+python -u train_city_custom.py ${CACHEPATH} ${SPECIF} ${LR} >csp_${SPECIF}.out
