eda500cfd75c572b9fc8e812019073620218c83e
vobecant <a.vobecky@gmail.com>
2020-08-12 23:31:00 +0200

    Training codes for Caltech.

---
+++ run_test_caltech_time.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+START=${1}
+END=${2}
+TIME=${3}
+EXPNAME="test_caltech_run2_${START}-${END}"
+JOB="./jobs/${EXPNAME}.job"
+
+echo "#!/bin/bash
+#SBATCH --job-name=tst_${START}-${END}
+#SBATCH --output=${EXPNAME}.err
+#SBATCH --time=3-00:00:00
+#SBATCH --begin=${TIME}
+#SBATCH --mem=20GB
+#SBATCH --gres=Volta100:1
+#SBATCH --exclude=node-16,node-12
+#SBATCH --cpus-per-task=16
+#SBATCH --partition=deadline
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=a.vobecky@gmail.com
+
+python -u test_caltech.py ${START} ${END} >${EXPNAME}.out" >${JOB}
+sbatch ${JOB}
+echo "Run job ${JOB}\n"
+
