e5a73868379562c96f8f15b9ca742f265168a712
vobecant <a.vobecky@gmail.com>
2020-08-12 21:36:50 +0200

    Training codes for Caltech.

---
+++ run_train_caltech_extended_1gpu.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=csp_caltech_extended_run2
+#SBATCH --output=csp_caltech_extended_run2.err
+#SBATCH --time=3-00:00:00
+#SBATCH --mem=190GB
+#SBATCH --gres=gpu:Volta100:8
+#SBATCH --cpus-per-task=16
+#SBATCH --partition=deadline
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=a.vobecky@gmail.com
+
+###############
+#   CALTECH   #
+###############
+# train the detector on Caltech
+CACHEPATH="data/cache/caltech/train_gt_ext"
+SPECIF=caltech_extended_run2
+LR=1e-4
+python -u train_caltech_extended.py ${CACHEPATH} ${SPECIF} ${LR} >csp_${SPECIF}.out
